84dc9946567f5462759715c516e15dacca09d7f4
miaCCC <ta828383@gmail.com>
2022-12-07 10:10:47 +0800

    基礎html建置，載入fontawesome

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,51 +2,68 @@
   <nav>
     <div class="nav-container">
       <div class="nav-title">
-        <font-awesome-icon icon="fa-solid fa-headphones-simple" />
+        <i class="fas fa-headset"></i>
         <h2>資訊支援服務台</h2>
-
+        <i class="fas fa-times"></i>
       </div>
+
       <div class="nav-user">
-        <img src="" alt="" class="avatar">
-        <div class="user-info">
-          <span class="name"></span>
-          <span class="number"></span>
-          <span class="admin"></span>
+        <img src="https://tw1.uetracktw.com/SupportTicket/deskhelp/Images/line.png" class="nav-user__avatar" />
+        <div class="nav-user__user-info">
+          <span class="name">張雅棠</span>
+          <span class="number">1989</span>
+          <span class="admin">Admin</span>
         </div>
-
       </div>
+
       <div class="nav-list">
         <ul>
-          <li class="home">
-            <router-link to="/home" class="">
-            Home
+          <li class="nav-list__home">
+            <router-link to="/home" class>
+              <i class="fas fa-home"></i>
+                <span>Home</span>            
             </router-link>
           </li>
-          <li class="add">
-            <router-link to="/add" class="">
-            建立案件
+          <li class="nav-list__add">
+            <router-link to="/add" class>
+              <i class="far fa-file-alt"></i>
+                <span>建立案件</span>
             </router-link>
           </li>
-          <li class="list">
-            <router-link to="/list" class="">
-            案件列表
+          <li class="nav-list__list">
+            <router-link to="/list" class>
+              <i class="fas fa-bug"></i>
+                <span>案件列表</span> 
             </router-link>
           </li>
-          <li class="setting">
-            <router-link to="/setting" class="">
-            管理設定
-            </router-link>
+          <li class="nav-list__setting">
+            <i class="fas fa-cog"></i>
+              <span>管理設定</span>
+            <div class="nav-list__menu">
+              <ul>
+                <li>問題類型管理</li>
+                <li>專案問題管理</li>
+                <li>專案類別管理</li>
+                <li>專案管理</li>
+                <li>地點管理</li>
+                <li>系統負責人管理</li>
+                <li>專員管理</li>
+                <li>人員管理</li>
+              </ul>
+            </div>
           </li>
-          <li class="forms">
-            <router-link to="/forms" class="">
-            報表
-            </router-link>
+          <li class="nav-list__menu nav-list__forms">
+            <i class="far fa-chart-bar"></i>
+            <span>報表</span>
+            <div class="nav-list__menu">
+              <ul>
+                <li>歷史案件查詢</li>
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
     </div>
-    <div class="cursor-pointer" style="position: absolute; bottom: 0" @click="logout">
-        登出
-    </div>    
+    <div class="cursor-pointer" @click="logout">登出</div>
   </nav>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,12 @@
 
  <div>
   <Navbar />
-  <h1>Home Page</h1>
  </div>
 
 </template>
 
 <script>
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 export default {
     components: { 
       Navbar
