8660b4b140ee8a113c6cfef55c582861365783ae
miaCCC <ta828383@gmail.com>
2022-12-07 10:51:23 +0800

    寫入scss基礎樣式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
+//載入assets/styles樣式設定
+import '@/assets/styles/main.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
